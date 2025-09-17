ea24de5dc902785ce025e0219a184656f6884337
Aryan <aryancoder03@gmail.com>
2025-09-17 20:04:51 +0530

    agentic rag workflow created

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -10,27 +10,51 @@ class Retriever:
     def __init__(self):
         """_summary_
         """
-        pass
+        self.model_loader = ModelLoader()
+        self.config = load_config()
+        self._load_env_variables()
+        self.vstore = None
+        self.retriever = None
 
     def _load_env_variables(self):
-        """_summary_
         """
-        pass
+        Load and validate required environment variables.
+        """
+        load_dotenv()
+        
+        required_vars = ["GOOGLE_API_KEY", "ASTRA_DB_API_ENDPOINT", "ASTRA_DB_APPLICATION_TOKEN", "ASTRA_DB_KEYSPACE"]
+        
+        missing_vars = [var for var in required_vars if os.getenv(var) is None]
+        if missing_vars:
+            raise EnvironmentError(f"Missing environment variables: {missing_vars}")
 
     def load_retriever(self):
         """_summary_
         """
-        pass
+        if not self.vstore:
+            collection_name = self.config["astra_db"]["collection_name"]
+            self.vstore = AstraDBVectorStore(
+                embedding= self.model_loader.load_embeddings(),
+                collection_name=collection_name,
+                api_endpoint=os.getenv("ASTRA_DB_API_ENDPOINT"),
+                token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
+                namespace=os.getenv("ASTRA_DB_KEYSPACE"),
+            )
+        if not self.retriever:
+            top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
+            retriever = self.vstore.as_retriever(search_type="similarity", search_kwargs={"k": top_k})
+            print("Retriever loaded successfully.")
+            return retriever
 
-    def call_retriever(self,user_query:str, top_k:int=3)
+    def call_retriever(self,user_query:str):
         """_summary_
         """
-        pass
-
+        retriever = self.load_retriever()
+        output = retriever.invoke(user_query)
+        return output
 if __name__ == "__main__":
     retriever_obj = Retriever()
     user_query = "what is the capital of India?"
     results = retriever_obj.call_retriever(user_query)
     for idx,doc in enumerate(results,1):
         print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
-        
